0adb5539397da02e9de0e3ce5eddbbc8b6867252
Arjun Aditya <meow@nermalcat69.dev>
2024-12-29 00:28:54 +0530

    iterate

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,17 +10,17 @@
 
   <!-- Open Graph / Facebook -->
   <meta property="og:type" content="website" />
-  <meta property="og:url" content="https://senate-des-jake-kept.trycloudflare.com/" />
+  <meta property="og:url" content="https://kritika.zerops.xyz/" />
   <meta property="og:title" content="Kritika Chaudhary" />
   <meta property="og:description" content="I'm a 18 year old Web Developer and I'm currently learning SvelteKit" />
-  <meta property="og:image" content="https://senate-des-jake-kept.trycloudflare.com/og.png" />
+  <meta property="og:image" content="https://kritika.zerops.xyz/og.png" />
 
   <!-- Twitter kritika.cc -->
   <meta property="twitter:card" content="summary_large_image" />
-  <meta property="twitter:url" content="https://senate-des-jake-kept.trycloudflare.com/" />
+  <meta property="twitter:url" content="https://kritika.zerops.xyz/" />
   <meta property="twitter:title" content="Kritika Chaudhary" />
   <meta property="twitter:description" content="I'm a 18 year old Web Developer and I'm currently learning SvelteKit" />
-  <meta property="twitter:image" content="https://senate-des-jake-kept.trycloudflare.com/og.png" />
+  <meta property="twitter:image" content="https://kritika.zerops.xyz/og.png" />
 </svelte:head>
 
 <slot />
