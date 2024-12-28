ec8b328ad9577bd2a259195d6e43ea7538590990
Arjun Aditya <meow@nermalcat69.dev>
2024-12-28 23:07:37 +0530

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+  import "../app.css";
+</script>
+
+<svelte:head>
+  <!-- Primary Meta Tags -->
+  <title>Kritika Chaudhary</title>
+  <meta name="title" content="Kritika Chaudhary" />
+  <meta name="description" content="I'm a 18 year old Web Developer and I'm currently learning SvelteKit" />
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://senate-des-jake-kept.trycloudflare.com/" />
+  <meta property="og:title" content="Kritika Chaudhary" />
+  <meta property="og:description" content="I'm a 18 year old Web Developer and I'm currently learning SvelteKit" />
+  <meta property="og:image" content="https://senate-des-jake-kept.trycloudflare.com/og.png" />
+
+  <!-- Twitter kritika.cc -->
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content="https://senate-des-jake-kept.trycloudflare.com/" />
+  <meta property="twitter:title" content="Kritika Chaudhary" />
+  <meta property="twitter:description" content="I'm a 18 year old Web Developer and I'm currently learning SvelteKit" />
+  <meta property="twitter:image" content="https://senate-des-jake-kept.trycloudflare.com/og.png" />
+</svelte:head>
+
+<slot />
